e44fbd2712dc614104946a7f99a24e1058c83540
BAILLOT STEPHANE <[stefbbst@gmail.com](stefbbst@gmail.com)>
2023-01-11 09:50:37 +0100

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SignupComponent } from './signup/signup.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SignupComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule
